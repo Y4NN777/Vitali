7fe14e5ae5996ffc8108d49a0ccc63e69d803c68
Y4NN777 <axel.studiesmail@gmail.com>
2025-07-28 12:23:59 +0000

    core: Project setup complete

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="vitali - medical-chatbot",
+    version="0.1.0",
+    author="Ragnang-Newende Yanis Axel DABO",
+    author_email="axeldaboworkplace@gmail.com",
+    description="A medical chatbot that provides information and advice on various health-related topics.",
+    long_description="This package contains a medical chatbot that can answer questions and provide information on various health-related topics. It is designed to assist users in understanding medical conditions, treatments, and general health advice.",
+    packages= find_packages(),
+    install_requires=[]
+)

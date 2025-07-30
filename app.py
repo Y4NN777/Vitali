9c5f590b1bda80e17fdff36dbd6d34dd61061ccf
Y4NN777 <axel.studiesmail@gmail.com>
2025-07-30 15:22:51 +0000

    Model: Switch model to mistral for better production UX experience

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
 chat_model = ChatOpenAI(
-    model="qwen/qwen3-235b-a22b-2507:free",
+    model="mistralai/mistral-7b-instruct",
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     openai_api_base="https://openrouter.ai/api/v1",
 )
